8c6252fc27255ad18f873c4ae5e52cedb4fb64b8
kirankumar13 <59715258+kirankumar13@users.noreply.github.com>
2020-12-19 07:02:26 +0530

    Upd

--- js/main.js
+++ js/main.js
@@ -105,8 +105,8 @@
 	$('#testimonial-mf').owlCarousel({
 		margin: 20,
 		autoplay: true,
+		loop:true,
 		autoplayTimeout: 4000,
-		autoplayHoverPause: true,
 		responsive: {
 			0: {
 				items: 1,
